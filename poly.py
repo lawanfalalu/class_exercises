625b70c058fa6055873f6380e88650d8d4fbd532
lawanfalalu <flawan@aust.edu.ng>
2017-05-24 23:31:55 +0100

    first_trial
    were there f[u-2] by f[u]

---
+++ poly.py
@@ -0,0 +1,25 @@
+#definition of a polynomial function
+import numpy as n
+def poly(min,max,step):
+    coeff=[];f=[];guess=[];u=0
+    degree = int(input("Enter the DEGREE : "))
+    for i in range(degree+1):
+        coeff.append(float(input("Enter coeffecient no. %d "%(i+1))))
+    vrange = n.arange(min,max,step)
+    for j in vrange:
+        #for k in coeff
+        sum=0
+        for k in range(degree+1):
+            sum = sum + coeff[k]*j**(degree-k)
+        f.append(sum)
+        if(u>1):
+            #if((f[u-1]*f[u])<0 and (f[u-1]*f[u])!=0) :
+            if(f[u-2]*f[u])<0:
+                guess.append(j)
+        u=u+1
+        
+        print("f(%f) = %f "%(j,sum))
+    print(guess)
+
+#Calling the method
+poly(-20,20,0.5)
