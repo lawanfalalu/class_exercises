d3e9ecf338d4c0dc7c6ec25d412c41737294fa51
lawanfalalu <flawan@aust.edu.ng>
2017-05-25 17:26:25 +0100

    updating Gauss iterations
    Uploading Gauss iterations

---
+++ gauss_iterations.py
@@ -0,0 +1,18 @@
+#Gauss iterartions
+print("j \t X1 \t X2 \t X3")
+y1=[];y2=[];y3=[]
+x1=4
+x2=-8
+x3=15
+for j in range(50):
+    x1 = (4+x2-3*x3)/2
+    y1.append(x1)
+    x2 = (-8-x1+2*x3)/9
+    y2.append(x2)
+    X3 = (15 - 4*x1 + 8*x2)/10
+    y3.append(x3)
+    print(j,"\t",x1,"\t",x2,"\t",x3)
+    if(j>0):
+        if((abs(y1[j]-y1[j-1]))<0.00001) and ((abs(y2[j]-y2[j-1]))<0.00001) and ((abs(y3[j]-y3[j-1]))<0.00001):
+            print("Converges at ",j)
+            break
